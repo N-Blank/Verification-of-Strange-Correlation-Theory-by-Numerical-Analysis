e835cb615f079fcc1aa445ef2bcbdb094d7daa90
N-Blank <95960176+N-Blank@users.noreply.github.com>
2023-11-13 17:46:52 +0800

    Update Calculation of  Strange Correlator with Order Parameter Detector with Z_2T Group in PBC.py

--- Verification of Strange Correlation Theory/Calculation of  Strange Correlator with Order Parameter Detector with Z_2T Group in PBC.py
+++ Verification of Strange Correlation Theory/Calculation of  Strange Correlator with Order Parameter Detector with Z_2T Group in PBC.py
@@ -1,29 +1,37 @@
 import numpy as np
-import sys
+import matplotlib.pyplot as plt
 from itertools import product
 
 # G is our Z2 group
-# N is the size of the system N != (4n + 2) or deternimnator = 0
+# N is the size of the system N != (4n + 2) or determinant = 0
 # d is the distance between two operators
 # confs is all configurations
 G = [0, 1]
 N = 13
 confs = list(product(G, repeat=N))
 
+d_values = range(N)
+strange_values = []
 
-for d in range(N):
+for d in d_values:
     strange = 0
     numer = 0
     deter = 0
     for conf in confs:
         coefficient = 1
-        for i in range(N):  
+        for i in range(N):
             if conf[i] > conf[(i + 1) % N]:
                 coefficient *= -1
         deter += coefficient
         if conf[0] != conf[d - 1]:
             coefficient *= -1
-        else:
-            coefficient *= 1
         numer += coefficient
-    print(f"strange ({d}) = {numer/deter}")  
+    strange_values.append(numer / deter)
+
+# Plotting the values
+plt.plot(d_values, strange_values)
+plt.xlabel('d')
+plt.ylabel('strange')
+plt.title('Plot of strange vs. d')
+plt.grid(True)
+plt.show()
